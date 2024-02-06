1e8b178127ea86de9b1b5c5772aaeb2c524769f9
Kadrizzle <lanekade8@gmail.com>
2024-02-06 01:30:12 -0600

    Setting up identities

--- Selu383.SP24.Api/Data/DataContext.cs
+++ Selu383.SP24.Api/Data/DataContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using Selu383.SP24.Api.Features.Hotels;
 
@@ -35,4 +37,15 @@ public class DataContext : DbContext
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
     }
+
+    public virtual ICollection<IdentityUserRole<int>> Roles { get; } = new List<IdentityUserRole<int>>();
+    public virtual ICollection<IdentityUserRole<int>> Users { get; } = new List<IdentityUserRole<int>>();
+    public virtual ICollection<IdentityRole<int>> Role { get; } = new List<IdentityRole<int>>();
+    public virtual ICollection<IdentityUser<int>> User { get; } = new List<IdentityUser<int>>();
+    public virtual ICollection<IdentityUserLogin<int>> Logins { get; } = new List<IdentityUserLogin<int>>();
+    public virtual ICollection<IdentityRoleClaim<int>> Claims { get; } = new List<IdentityRoleClaim<int>>();
+    public virtual ICollection<IdentityUserToken<int>> Tokens { get; } = new List<IdentityUserToken<int>>();
+
+
+
 }
