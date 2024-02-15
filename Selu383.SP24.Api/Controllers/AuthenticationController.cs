2582345da93861afdfa911beffadc69fb38f2aef
Kadrizzle <lanekade8@gmail.com>
2024-02-14 23:32:28 -0600

    Adding me endpoint on authentication controller

--- Selu383.SP24.Api/Controllers/AuthenticationController.cs
+++ Selu383.SP24.Api/Controllers/AuthenticationController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Selu383.SP24.Api.Data;
 using Selu383.SP24.Api.Features.Hotels;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
 
 namespace Selu383.SP24.Api.Controllers
 {
@@ -15,6 +20,28 @@ namespace Selu383.SP24.Api.Controllers
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
+        }
+
+        [HttpGet("me")]
+        [Authorize]
+        public async Task<ActionResult<UserDto>> Me(User user)
+        {
+            var username = GetCurrentUserName(user);
+
+            if (string.IsNullOrEmpty(username))
+            {
+                return Unauthorized();
+            }
+
+            var resultDto = await GetUserDto(userManager.Users).SingleAsync
+                (x => x.UserName == username);
+
+            return Ok(resultDto);
+        }
+
+        private string? GetCurrentUserName(ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.Identity?.Name;
         }
     }
 }
