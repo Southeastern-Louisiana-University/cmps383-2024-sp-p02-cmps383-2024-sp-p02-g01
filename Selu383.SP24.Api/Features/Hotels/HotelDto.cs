4ce2c12856afee0f4461c3d3a4b07d302b8b8e1c
Kadrizzle <lanekade8@gmail.com>
2024-02-14 22:44:11 -0600

    fixing errors in hoteldto and adding managerId

--- Selu383.SP24.Api/Features/Hotels/HotelDto.cs
+++ Selu383.SP24.Api/Features/Hotels/HotelDto.cs
@@ -4,7 +4,8 @@ public class HotelDto
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
-    public string Address { get; set; }
+    public string Address { get; set; } = string.Empty;
+    public int? ManagerId { get; set; }
 }
