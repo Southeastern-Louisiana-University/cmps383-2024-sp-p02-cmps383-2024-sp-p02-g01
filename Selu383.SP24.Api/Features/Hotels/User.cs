e805a8b447cdef1a9066d42c1dd67ba6ad611634
Kadrizzle <lanekade8@gmail.com>
2024-02-14 22:14:38 -0600

    Adding Dto to User

--- Selu383.SP24.Api/Features/Hotels/User.cs
+++ Selu383.SP24.Api/Features/Hotels/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Selu383.SP24.Api.Features.Hotels
 {
@@ -6,5 +7,21 @@ namespace Selu383.SP24.Api.Features.Hotels
     {
         //public override int Id { get; set; }
         public ICollection<UserRole>? Roles { get; set; }
+    }
+
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string[] Roles { get; set; } = Array.Empty<string>();
+    }
+
+    public class CreateUserDto
+    {
+        [Required] public string UserName { get; set; } = string.Empty;
+
+        [Required] public string Password { get; set; } = string.Empty;
+
+        [Required] public string[] Roles { get; set; } = Array.Empty<string>();
     }
 }
