136610a5be7472d194f36a2ffa5141e5d2a8ba3f
Kadrizzle <lanekade8@gmail.com>
2024-02-14 22:10:19 -0600

    Adding authentication controller

---
+++ Selu383.SP24.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Selu383.SP24.Api.Features.Hotels;
+
+namespace Selu383.SP24.Api.Controllers
+{
+    [ApiController]
+    [Route("api/authentication")]
+    public class AuthenticationController
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AuthenticationController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+    }
+}
