f748e446b8bb5458a2daf6fc8491340cf62cf503
Kadrizzle <lanekade8@gmail.com>
2024-02-14 23:30:43 -0600

    Adding SeedHelper class which seeds data

---
+++ Selu383.SP24.Api/Data/SeedHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Selu383.SP24.Api.Features.Hotels;
+
+namespace Selu383.SP24.Api.Data
+{
+    public class SeedHelper
+    {
+        private static async Task AddUsers(IServiceProvider serviceProvider)
+        {
+            const string defaultPass = "Password123!";
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+            if (userManager.Users.Any())
+            {
+                return;
+            }
+
+            var adminUser = new User
+            {
+                UserName = "galkadi",
+            };
+
+            var result = await userManager.CreateAsync(adminUser, defaultPass);
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, RoleNames.Admin);
+            }
+            var bob = new User
+            {
+                UserName = "bob",
+            };
+
+            await userManager.CreateAsync(bob, defaultPass);
+            await userManager.AddToRoleAsync(bob, RoleNames.User);
+
+            var sue = new User
+            {
+                UserName = "sue",
+            };
+
+            await userManager.CreateAsync(sue, defaultPass);
+            await userManager.AddToRoleAsync(sue, RoleNames.User);
+
+        }
+    }
+}
